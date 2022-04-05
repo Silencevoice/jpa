39edc36e9a5aaf600fa7428e3760b0727e232343
jaime.alvarezvilloria <jaime.alvarezvilloria@wps-nl.com>
2022-04-05 17:01:45 +0200

    Added test for persist and found

--- src/test/java/es/alten/adc/jpa/TestPersistenceContext.java
+++ src/test/java/es/alten/adc/jpa/TestPersistenceContext.java
@@ -1,21 +1,29 @@
 package es.alten.adc.jpa;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import es.alten.adc.jpa.entity.Gender;
+import es.alten.adc.jpa.entity.Student;
 
 public class TestPersistenceContext {
 	
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-training");
 	
 	private EntityManager entityManager;
-	
+
 	@Before
 	public void setup() {
 		entityManager = emf.createEntityManager();
@@ -25,6 +33,30 @@ public class TestPersistenceContext {
 	public void givenEntityManagerFactory_whenCreateEntityManager_thenNotNull() {
 		
 		assertNotNull("entityManager is null", entityManager);
+	}
+	
+	@Test
+	public void givenNewStudent_whenSave_thenFound() {
+		// Given new Student
+		final Student student = new Student();
+		student.setName("Agnes Alcántara");
+		student.setGender(Gender.FEMALE);
+		final Calendar calendar = new GregorianCalendar();
+		calendar.set(Calendar.YEAR, 1970);
+		calendar.set(Calendar.MONTH, Calendar.JANUARY);
+		calendar.set(Calendar.DATE, 1);
+		student.setBirthDate(calendar.getTime());
+		
+		// When save
+		entityManager.getTransaction().begin();
+		entityManager.persist(student);
+		entityManager.getTransaction().commit();
+		
+		// Then found
+		assertNotNull("Persisted student has null Id", student.getId());
+		final Student foundStudent = entityManager.find(Student.class, student.getId());
+		assertNotNull("Student not found", foundStudent);
+		assertEquals("Found student not equals to original", student, foundStudent);
 	}
 	
 	@After
