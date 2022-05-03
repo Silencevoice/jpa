02b8aadbd1e0c32bd1dba74cab68a6bb13bda282
jaime.alvarezvilloria <jaime.alvarezvilloria@wps-nl.com>
2022-05-03 10:03:06 +0200

    Solved tests

--- src/test/java/es/alten/adc/jpa/TestPersistenceContext.java
+++ src/test/java/es/alten/adc/jpa/TestPersistenceContext.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertNotNull;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import es.alten.adc.jpa.entity.Gender;
